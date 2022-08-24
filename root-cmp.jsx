dbedece97d97ab3f0bb33844d1a769b6eb93a99c
Rebecca Kruk <rebeccakruk@gmail.com>
2022-08-24 10:01:13 +0300

    app skeleton

--- root-cmp.jsx
+++ root-cmp.jsx
@@ -3,6 +3,7 @@ import { About } from "./views/about.jsx"
 import { Home } from "./views/home.jsx"
 import { MailIndex } from "./apps/mail/views/mail-index.jsx"
 import { NoteIndex } from "./apps/note/views/note-index.jsx"
+import { AppList } from "./cmps/app-list.jsx"
 
 const Router = ReactRouterDOM.HashRouter
 const { Route, Switch } = ReactRouterDOM
--- views/home.jsx
+++ views/home.jsx
@@ -1,7 +1,10 @@
+import { AppList } from "../cmps/app-list.jsx"
+
 
 export function Home() {
 
     return <section className="home">
         <h1>Welcome to home page!</h1>
+        <AppList />
     </section>
 }
