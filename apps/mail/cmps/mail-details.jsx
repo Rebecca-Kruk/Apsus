29a5a45afd43ebd90b1b10e9c442b0dce8e3f8e1
TanyaGr9191 <tania91.gr@gmail.com>
2022-08-25 17:54:42 +0300

    added css  to mail app

--- apps/mail/cmps/mail-details.jsx
+++ apps/mail/cmps/mail-details.jsx
@@ -1,22 +1,15 @@
-import { utilService } from "../../../services/util.service.js"
 
-export function EmailDetails({ email, logggedinUser, onRemoveEmail }) {
+export function EmailDetails({ email, logggedinUser }) {
 
-    // console.log('email from Email Setails', email)
-    // console.log('logggedinUser from Email Setails', logggedinUser)
     return <section className="mail-details">
-        <hr />
-        <span>from: {logggedinUser.email}</span><br />
-        <span>to: {email.to}</span><br />
-        <hr />
-        <div className="mail-content">
+        <div className="mail-details-header">
+            <span>from: {logggedinUser.email}</span>
+            <span>to: {email.to}</span>
+        </div>
+        <div className="mail-details-body">
             <span>
                 {email.body}
             </span>
-            <div>
-                <button className="btn-remove" onClick={() => onRemoveEmail(email.id)}><i className="fa-solid fa-trash-can"></i></button>
-                <button className="btn-edit"><i className="fa-solid fa-pencil"></i></button>
-            </div>
         </div>
     </section>
 }
--- apps/mail/cmps/mail-preview.jsx
+++ apps/mail/cmps/mail-preview.jsx
@@ -16,18 +16,13 @@ export class EmailPreview extends React.Component {
     render() {
         const { email, logggedinUser, isSelected } = this.state
         const { onSelectToggle } = this
-        // console.log('Email from emailPreview', email)
-        // console.log('User from emailPreview', logggedinUser)
 
         return <article className="mail-preview">
             <div className="mail-preview-container" onClick={onSelectToggle}>
                 <span className="fullname">{logggedinUser.fullname}</span><br />
                 <span className="subject">{email.subject}</span><br />
                 <span className="sentAt">{utilService.createdAt(email.sentAt)}</span><br />
-                <div className="btn-mail-preview">
-                    <button className="btn-remove" onClick={() => this.props.onRemoveEmail(email.id)}><i className="fa-solid fa-trash-can"></i></button>
-                    {/* <button className="btn-edit" ><i className="fa-solid fa-pencil"></i></button> */}
-                </div>
+                <button className="btn-remove" onClick={() => this.props.onRemoveEmail(email.id)}><i className="fa-solid fa-trash-can"></i></button>
             </div>
             {isSelected && <EmailDetails email={email} logggedinUser={logggedinUser} onRemoveEmail={this.props.onRemoveEmail} />}
         </article>
