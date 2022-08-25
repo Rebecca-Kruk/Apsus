24a84326fbcd6f4546dc3a9906bcb3690f64274f
Rebecca Kruk <rebeccakruk@gmail.com>
2022-08-25 18:35:22 +0300

    tring to fix img url path

--- apps/note/services/note.service.js
+++ apps/note/services/note.service.js
@@ -106,7 +106,7 @@ function _getNotes() {
             // isPinned: true,
             info: {
                 // url: 'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80',
-                url: '../../assets/img/note/test.jpg',
+                url: '../../../assets/img/note/test.jpg',
                 title: 'Bobi and Me'
             },
             classBgColor: '',
