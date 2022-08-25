e116f823ce98b624184d41e696bb3d4008a97588
TanyaGr9191 <tania91.gr@gmail.com>
2022-08-25 14:02:18 +0300

    email-list-btn

--- apps/mail/cmps/mail-preview.jsx
+++ apps/mail/cmps/mail-preview.jsx
@@ -26,7 +26,7 @@ export class EmailPreview extends React.Component {
                 <span className="sentAt">{utilService.createdAt(email.sentAt)}</span><br />
                 <div className="btn-mail-preview">
                     <button className="btn-remove" onClick={() => this.props.onRemoveEmail(email.id)}><i className="fa-solid fa-trash-can"></i></button>
-                    <button className="btn-edit" ><i className="fa-solid fa-pencil"></i></button>
+                    {/* <button className="btn-edit" ><i className="fa-solid fa-pencil"></i></button> */}
                 </div>
             </div>
             {isSelected && <EmailDetails email={email} logggedinUser={logggedinUser} onRemoveEmail={this.props.onRemoveEmail} />}
